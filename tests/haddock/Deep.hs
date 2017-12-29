cb4731544341dd8d74a82f051ee13d1864f9ed46
Mateusz Kowalczyk <mateusz.kowalczyk@tweag.io>
2017-12-29 11:23:14 +0000

    Verify that deeper dependencies work with haskell_haddock.
    Closes #54, closes #50.

---
+++ tests/haddock/Deep.hs
@@ -0,0 +1,6 @@
+-- | "Deep" doc.
+module Deep (deep_lib) where
+
+-- | 'deep_lib' doc.
+deep_lib :: ()
+deep_lib = ()
--- tests/haddock/LibA.hs
+++ tests/haddock/LibA.hs
@@ -2,12 +2,13 @@
 module LibA where
 
 import LibA.A (a)
+import Deep (deep_lib)
 
 -- | 'A' declaration.
 data A =
   -- | 'A' constructor.
   A
 
--- | Doc for 'f' using 'a'.
+-- | Doc for 'f' using 'a' and 'deep_lib'.
 f :: ()
-f = a
+f = const a deep_lib
