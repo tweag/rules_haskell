ed9c75af4e805dca8890ef9fe65a3108eaf1bb0f
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2018-06-07 17:50:00 +0200

    Remove the sub workspace targets-that-must-fail
    It was created to avoid failure of `bazel build //...`, but `manual` tag
    can be used instead

--- tests/targets-that-must-fail/run-tests.sh
+++ tests/targets-that-must-fail/run-tests.sh
@@ -5,15 +5,15 @@ set -e
 test_transitive_deps() {
     set +e
 
-    bazel build @targets_that_must_fail//transitive-deps:lib-c
+    bazel build //tests/targets-that-must-fail/transitive-deps:lib-c
     if [ $? -eq 0 ]; then
-        echo "bazel build @targets_that_must_fail//transitive-deps:lib-c should have failed."
+        echo "bazel build //tests/targets-that-must-fail/transitive-deps:lib-c should have failed."
         exit 1
     fi
 
-    bazel build @targets_that_must_fail//transitive-deps:lib-d
+    bazel build //tests/targets-that-must-fail/transitive-deps:lib-d
     if [ $? -eq 0 ]; then
-        echo "bazel build @targets_that_must_fail//transitive-deps:lib-d should have failed."
+        echo "bazel build //tests/targets-that-must-fail/transitive-deps:lib-d should have failed."
         exit 1
     fi
 
