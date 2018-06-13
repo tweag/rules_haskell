acd0782b38524c3ac0c72d1c91cc4febadad18e4
Mathieu Boespflug <m@tweag.io>
2018-06-13 16:03:26 +0200

    Comment on OOM issue we're working around.

--- shell.nix
+++ shell.nix
@@ -5,6 +5,8 @@ with pkgs;
 mkShell {
   buildInputs = [
     binutils
+    # XXX Prepopulating the shell with ghc to workaround OOM conditions
+    # caused by https://github.com/NixOS/nix/issues/1969.
     ghc
     go
     which
