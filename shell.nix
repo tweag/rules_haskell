1b58f415d2e0b0b1456c2dc55292be62643344d6
Mathieu Boespflug <m@tweag.io>
2018-06-13 19:16:52 +0200

    Reposition workaround for https://github.com/NixOS/nix/issues/1969

--- shell.nix
+++ shell.nix
@@ -5,9 +5,6 @@ with pkgs;
 mkShell {
   buildInputs = [
     binutils
-    # XXX Prepopulating the shell with ghc to workaround OOM conditions
-    # caused by https://github.com/NixOS/nix/issues/1969.
-    ghc
     go
     which
     python
