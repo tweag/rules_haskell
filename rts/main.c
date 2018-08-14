887996082f47f8d04ea4b46d0c289491ad46d6c4
Mathieu Boespflug <m@tweag.io>
2018-08-14 07:39:58 +0200

    Fix C warning.

--- rts/main.c
+++ rts/main.c
@@ -5,7 +5,7 @@ extern HsInt add_one_hs(HsInt a0);
 
 int main(int argc, char *argv[]) {
   hs_init(&argc, &argv);
-  printf("Adding one to 5 through Haskell is %d\n", add_one_hs(5));
+  printf("Adding one to 5 through Haskell is %ld\n", add_one_hs(5));
   hs_exit();
   return 0;
 }
