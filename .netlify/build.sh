6b2978a1d3403c9494ba37061d3985e90c7df90a
Mathieu Boespflug <m@tweag.io>
2018-08-14 15:53:09 +0200

    --host_copt=-O0 doesn't save us build time. So let's do away with it.

--- .netlify/build.sh
+++ .netlify/build.sh
@@ -4,8 +4,6 @@ set -eux
 
 export PATH=$HOME/bin:$PATH
 
-# Host configuration has optimizations turned on by default. Turn them
-# off to save time.
-bazel build --host_copt=-O0 //docs
+bazel build //docs
 unzip -d public bazel-bin/docs/docs-skydoc.zip
 cp start public
