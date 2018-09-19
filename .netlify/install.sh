11a4c0eb9c3a1bb71229495448acaf2667020129
Théophane Hufschmitt <theophane.hufschmitt@tweag.io>
2018-09-20 01:28:34 +0200

    Fix netlify deployment

--- .netlify/install.sh
+++ .netlify/install.sh
@@ -7,3 +7,18 @@ V=0.16.0
 curl -LO https://github.com/bazelbuild/bazel/releases/download/$V/bazel-$V-installer-linux-x86_64.sh
 chmod +x bazel-$V-installer-linux-x86_64.sh
 ./bazel-$V-installer-linux-x86_64.sh --user
+
+# XXX: Hack to prevent the `gen_packages_list` rule from crashing
+packages_list=$(mktemp -d)
+cat <<EOF > $packages_list/all-haskell-packages.bzl
+packages = []
+EOF
+
+mkdir -p $HOME
+cat <<EOF > $HOME/bin/nix-build
+#!/usr/bin/env bash
+
+echo $packages_list
+EOF
+
+chmod +x $HOME/bin/nix-build
