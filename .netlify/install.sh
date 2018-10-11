79267dcb71f8184fb8f274bb507041b6657217e5
Théophane Hufschmitt <theophane.hufschmitt@tweag.io>
2018-10-11 10:08:15 +0200

    netlify: Update comment on dummy `nix-build`

--- .netlify/install.sh
+++ .netlify/install.sh
@@ -8,7 +8,11 @@ curl -LO https://github.com/bazelbuild/bazel/releases/download/$V/bazel-$V-insta
 chmod +x bazel-$V-installer-linux-x86_64.sh
 ./bazel-$V-installer-linux-x86_64.sh --user
 
-# XXX: Hack to prevent the `gen_packages_list` rule from crashing
+# XXX: Hack to prevent the `haskell_nixpkgs_package_list` rule from crashing:
+# This rule expects a `nix-build` executable which is used to generate a
+# store-path containing an `all-haskell-packages.bzl` file which defines the
+# `package` list. Since actually installing `nix-build` on the netlify image
+# seems difficult, we provide a dummy shell script which does exactly that.
 packages_list=$(mktemp -d)
 cat <<EOF > $packages_list/all-haskell-packages.bzl
 packages = []
