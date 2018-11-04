8c0132ac5d625585f61c46d4fd0c7d0a1417dfcf
Mathieu Boespflug <m@tweag.io>
2018-11-05 00:13:14 +0100

    Annotate Nixpkgs version with date.

--- nixpkgs/default.nix
+++ nixpkgs/default.nix
@@ -1,4 +1,5 @@
 import (fetchTarball {
+   # Nixpkgs checkout from 2018-11-03.
    url = "https://github.com/NixOS/nixpkgs/archive/ef4d78adedcfbb69392e7634bc6912c019b88bcc.tar.gz";
    sha256 = "0xmlklvh1as83fcfmy169m62kaj9w7nfm8y1c9x3ws29cjrpypf0";
 })
