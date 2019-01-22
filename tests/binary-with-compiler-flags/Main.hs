daecd7292cb666530236c6cd4798f59add0f212e
Gaël Deest <gael.deest@tweag.io>
2019-01-22 12:11:53 +0100

    Pass -XLambdaCase via compiler_flags and validate in test

--- tests/binary-with-compiler-flags/Main.hs
+++ tests/binary-with-compiler-flags/Main.hs
@@ -4,5 +4,10 @@ module Main (main) where
 data Foo = Foo
 deriving instance Show Foo
 
+-- Expects -XLambdaCase to be passed via the 'compiler_flags' rule attribute.
+dummyId :: Foo -> Foo
+dummyId = \case
+  Foo -> Foo
+
 main :: IO ()
-main = print Foo
+main = print $ dummyId Foo
