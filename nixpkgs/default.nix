300cc70372b1d0fcaa058c4ae6b726a018da0f60
Guillaume Bouchard <guillaume.bouchard@tweag.io>
2019-02-11 10:28:52 +0100

    bazel 0.22 WIP
    - just an updated nixpkgs for now

--- nixpkgs/default.nix
+++ nixpkgs/default.nix
@@ -1,5 +1,6 @@
 import (fetchTarball {
-   # Nixpkgs checkout from 2019-01-19.
-   url = "https://github.com/NixOS/nixpkgs/archive/db8e3654e2cdbc4bb32a5e75db064e0fea45a764.tar.gz";
-   sha256 = "1vfyv6p841b0pzxmpn4ch9xar8nbvg6hlb7g2yg48fjrzssmq1qn";
+   # Nixpkgs checkout from 2019-01-30.
+
+   url = https://github.com/NixOS/nixpkgs/archive/a5aca72f93715ea7a44e47028ed1404ec1efb525.tar.gz;
+   sha256 = "1k0ggn431z2aj4snq815w788qz4cw3ajs2wgnbhl0idqzqq6gm36";
 })
