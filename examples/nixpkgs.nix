7352f7f03539b0138f03791cbfe3356807c78a7a
Philip Patsch <philip.patsch@tweag.io>
2019-03-07 19:23:34 +0100

    Remove examples nixpkgs pin
    We use the global `rules_haskell` pin from now on.

--- examples/nixpkgs.nix
+++
@@ -1,5 +0,0 @@
-# Keep this value in sync with `WORKSPACE`
-import (builtins.fetchTarball {
-   url = "https://github.com/NixOS/nixpkgs/archive/9a787af6bc75a19ac9f02077ade58ddc248e674a.tar.gz";
-   sha256 = "17kp82r4dzcgsmjfzspkjhm8avny542lq6qcgqr9dkwbv1x1vyk0";
-})
