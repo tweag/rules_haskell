b347eae21f0782df148c87d4d04605a5fcc7bed2
Mathieu Boespflug <m@tweag.io>
2019-04-06 08:57:33 +0200

    Switch to a newer Nixpkgs, Bazel and GHC.

--- nixpkgs/default.nix
+++ nixpkgs/default.nix
@@ -1,6 +1,6 @@
 import (fetchTarball {
    # Nixpkgs checkout from 2019-01-30.
 
-   url = https://github.com/NixOS/nixpkgs/archive/a5aca72f93715ea7a44e47028ed1404ec1efb525.tar.gz;
-   sha256 = "1k0ggn431z2aj4snq815w788qz4cw3ajs2wgnbhl0idqzqq6gm36";
+   url = https://github.com/NixOS/nixpkgs/archive/2e2ab461b76ec2c586ab126acc53f53bce49cb94.tar.gz;
+   sha256 = "17gicjn4cafpajczv49czy5r61w16kplkbz6r8hqn5rkvp4i5i9b";
 })
