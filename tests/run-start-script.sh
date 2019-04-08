1af821b0957797e2bce9c3cc1ffd77cf110c2eb1
Philip Patsch <philip.patsch@tweag.io>
2019-04-08 14:28:15 +0200

    start script: Fix rules_haskell version & add CI override
    I accidentally pushed my WIP override in the start script PR.
    
    The CI intentionally overrode the repository, to test the current
    branch instead of the last release (otherwise e.g. bazel versions
    could be different and lead to failing CI).
    
    Closes: https://github.com/tweag/rules_haskell/issues/815

--- tests/run-start-script.sh
+++ tests/run-start-script.sh
@@ -14,8 +14,17 @@ mkdir tools
 cp $pwd/.bazelrc .bazelrc
 
 # Set Nixpkgs in environment variable to avoid hardcoding it in
-# start script itself
+# start script itself.
+
+# overrides the used rules_haskell, because
+# when we're testing the start on a feature branch (CI),
+# the latest rules_haskell version doesn't always work.
+# If on the branch we update Bazel to a version with breaking
+# changes, then we need to adapt to those changes in the branch.
+# Which in turn means the start script should pull in those changes too.
+
 NIX_PATH=nixpkgs=$pwd/nixpkgs/default.nix \
   bazel build \
   --config=ci \
+  --override_repository=io_tweag_rules_haskell=$pwd \
   //:example
