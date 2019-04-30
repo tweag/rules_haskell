5730137d689aba41d66947b63e87b0666dba269a
Andreas Herrmann <andreas.herrmann@tweag.io>
2019-04-30 11:56:35 +0200

    Hazel: Don't run ./configure in repository rule
    This violates hermeticity, as the ./configure script might pick up a
    system compiler, and system libraries, which are later replaced through
    library dependencies or cc toolchains.

--- hazel/hazel_base_repository/cabal2bazel.hs
+++ hazel/hazel_base_repository/cabal2bazel.hs
@@ -13,23 +13,18 @@ module Main (main) where
 
 #if MIN_VERSION_Cabal(2,2,0)
 import Distribution.PackageDescription.Parsec
-    (readGenericPackageDescription, parseHookedBuildInfo, runParseResult)
-import qualified Data.ByteString.Char8 as BS.C8
+    (readGenericPackageDescription)
 #else
 import Distribution.PackageDescription.Parse
-    (readGenericPackageDescription, parseHookedBuildInfo, ParseResult(..))
+    (readGenericPackageDescription)
 #endif
 
-import Distribution.Text (display, simpleParse)
+import Distribution.Text (simpleParse)
 import Distribution.Verbosity (normal)
 import System.Environment (getArgs)
-import System.FilePath ((<.>))
-import System.Process (callProcess)
 
 import qualified Data.Map.Strict as Map
-import qualified Distribution.Package as P
 import qualified Distribution.PackageDescription as P
-import qualified System.Directory as Directory
 
 import Description
 import Flatten
@@ -43,7 +38,7 @@ main = do
                       Nothing -> error $ "Error parsing ghc version: " ++ show ghcVersionStr
                       Just v -> v
         packageFlags = parseFlags flagArgs
-    desc <- maybeConfigure $ flattenToDefaultFlags ghcVersion packageFlags gdesc
+        desc = flattenToDefaultFlags ghcVersion packageFlags gdesc
     writeFile outFile $ show $ renderStatements
         [Assign "package" $ packageDescriptionExpr desc]
 
@@ -52,37 +47,3 @@ parseFlags = \case
   "-flag-on":flag:etc -> Map.insert (P.mkFlagName flag) True (parseFlags etc)
   "-flag-off":flag:etc -> Map.insert (P.mkFlagName flag) False (parseFlags etc)
   _ -> Map.empty
-
-maybeConfigure :: P.PackageDescription -> IO P.PackageDescription
-maybeConfigure desc = whenConfiguring $ do
-    callProcess "./configure" []
-    let buildInfoFile = display (P.packageName desc) <.> "buildinfo"
-    buildInfoExists <- Directory.doesFileExist buildInfoFile
-    if buildInfoExists
-      then processBuildInfoFile buildInfoFile
-      else return desc
-  where
-#if MIN_VERSION_Cabal(2,2,0)
-    whenConfiguring m = case P.buildType desc of
-      P.Configure -> m
-      _           -> return desc
-
-    processBuildInfoFile buildInfoFile = do
-      cs <- BS.C8.readFile buildInfoFile
-      case runParseResult (parseHookedBuildInfo cs) of
-        (_warnings, Left (_maybeVersion, es)) ->
-          error $ "Error reading buildinfo " ++ show buildInfoFile ++ ": " ++ show es
-        (_warnings, Right hookedBI) ->
-          return $ P.updatePackageDescription hookedBI desc
-#else
-    whenConfiguring m = case P.buildType desc of
-      Just P.Configure -> m
-      _                -> return desc
-
-    processBuildInfoFile buildInfoFile = do
-      cs <- readFile buildInfoFile
-      case parseHookedBuildInfo cs of
-          ParseFailed e -> error $ "Error reading buildinfo " ++ show buildInfoFile
-                                      ++ ": " ++ show e
-          ParseOk _ hookedBI -> return $ P.updatePackageDescription hookedBI desc
-#endif
