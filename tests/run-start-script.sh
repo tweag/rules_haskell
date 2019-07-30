1c16ca0d2e6a95b5da11b3f77bd340e77ce1a505
Philip Patsch <philip.patsch@tweag.io>
2019-07-30 16:38:07 +0200

    tests/run-start-script.sh: use start script .bazelrc
    We want to test the files, not the ones we use in the `rules_haskell`
    repository. If the generated files are not enough to run the example
    on CI, we should improve the generated files instead.

--- tests/run-start-script.sh
+++ tests/run-start-script.sh
@@ -15,10 +15,6 @@ cd $workdir
 # arguments are passed on to the start script
 $pwd/start "$@"
 
-# Copy the bazel configuration, this is only useful for CI
-mkdir tools
-cp $pwd/.bazelrc .bazelrc
-
 # Set Nixpkgs in environment variable to avoid hardcoding it in
 # start script itself.
 
