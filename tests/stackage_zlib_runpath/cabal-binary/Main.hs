f1b9ef186934f8d8ca9293a6222687ddd4d35a6c
Hans Hoeglund <hans@hanshoglund.se>
2020-06-19 18:33:30 +0100

    Add reproduction of issue 1365

--- tests/stackage_zlib_runpath/cabal-binary/Main.hs
+++ tests/stackage_zlib_runpath/cabal-binary/Main.hs
@@ -1,9 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Codec.Compression.Zlib (compress)
-
 main :: IO ()
-main = do
-  case compress "" of
-    _ -> pure ()
+main = pure ()
