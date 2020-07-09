839c5c0ecd15e0f14d6f77d2fd0abecdea078e32
Andreas Herrmann <andreas.herrmann@tweag.io>
2020-07-09 13:04:33 +0200

    upgrade Bazel 2.1.0 --> 3.3.1

--- nixpkgs/default.nix
+++ nixpkgs/default.nix
@@ -1,7 +1,7 @@
 let
-  # 2020-05-25
-  sha256 = "0ncsh5rkjbcdaksngmn7apiq1qhm5z1z6xa1x70svxq91znibp4f";
-  rev = "571212eb839d482992adb05479b86eeb6a9c7b2f";
+  # 2020-07-09
+  sha256 = "1vi3wbvlvpd4200swd3594vps1fsnd7775mgzm3nnfs1imzkg00i";
+  rev = "1d8018068278a717771e9ec4054dff1ebd3252b0";
 in
 import (fetchTarball {
   inherit sha256;
