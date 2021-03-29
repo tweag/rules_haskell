9ddbe98536f0c01f7b04bf095e73af09a1db845e
Yves-Stan Le Cornec <yves.stan.lecornec@tweag.io>
2021-03-29 15:34:31 +0200

    (#469) remove mentions of BAZEL_USE_CPP_ONLY_TOOLCHAIN

--- shell.nix
+++ shell.nix
@@ -2,12 +2,6 @@
 
 with pkgs;
 mkShell {
-  # XXX: hack for macosX, this flags disable bazel usage of xcode
-  # Note: this is set even for linux so any regression introduced by this flag
-  # will be catched earlier
-  # See: https://github.com/bazelbuild/bazel/issues/4231
-  BAZEL_USE_CPP_ONLY_TOOLCHAIN=1;
-
   # Set UTF-8 local so that run-tests can parse GHC's unicode output.
   LANG="C.UTF-8";
 
