51a7f97e6480bc19dd04b435e3c3d2de2b934b01
Yves-Stan Le Cornec <yves.stan.lecornec@tweag.io>
2021-04-12 17:06:21 +0200

    add tests for hie_bios_path_prefix argument of haskell_repl

---
+++ tests/hie-bios/test_hie_bios.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# To test the hie_bios_path_prefix argument of haskell_repl rule.
+# we check that the prefix $magic_string is added to some paths of file $1
+# and that if we remove them, we get back file $2
+
+if grep -q '$magic_string' "$1"; then
+    sed -i 's#$magic_string/##' "$1"
+    cmp "$1" "$2"
+else
+    echo "\$magic_string should be in file: $1"
+    exit 1
+fi
