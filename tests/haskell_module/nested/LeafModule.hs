11f7742ac61927b6f0bcf6378cbdbe27fcbc88e8
Andreas Herrmann <andreas.herrmann@tweag.io>
2021-05-10 16:49:35 +0200

    haskell_module nested hierarchy test

---
+++ tests/haskell_module/nested/LeafModule.hs
@@ -0,0 +1,7 @@
+module LeafModule where
+
+import Branch.Left.Module
+import Branch.Right.Module
+
+leaf :: Int
+leaf = 7 * branch_left * branch_right
---
+++ tests/haskell_module/nested/src/Branch/Right/Module.hs
@@ -0,0 +1,6 @@
+module Branch.Right.Module where
+
+import Root.Module
+
+branch_right :: Int
+branch_right = 5 * root
