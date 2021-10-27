0d77ad25861fec86f98dfb9db3f928706ca3d00f
Facundo Domínguez <facundo.dominguez@tweag.io>
2021-10-27 17:04:34 -0300

    wip test th

--- tests/haskell_module/multiple/BranchLeft.hs
+++ tests/haskell_module/multiple/BranchLeft.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE TemplateHaskell #-}
 module BranchLeft(branch_left, root) where
 
+import Language.Haskell.TH
 import Root
 
 branch_left :: Int
-branch_left = 3 * root
+branch_left = $(runIO (print root) >> [| 3 * root |])
