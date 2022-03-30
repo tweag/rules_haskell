51d4ba8f6bd8fb6cb3c8e73d5dec0a319e11d90a
Facundo Domínguez <facundo.dominguez@tweag.io>
2022-03-30 19:18:52 -0300

    Read requests with the ProtoClient

--- tools/haskell_module_worker/src/Options.hs
+++ tools/haskell_module_worker/src/Options.hs
@@ -1,23 +1,32 @@
-module Options (parseArgs) where
+module Options (Options(..), parseArgs) where
 
 import Data.Word (Word64)
 import System.Exit (ExitCode(ExitFailure), exitSuccess, exitWith)
 import System.IO (hPutStrLn, stderr)
 
--- | Parse options intended for the worker
---
--- Yields the options intended for GHC
-parseArgs :: [String] -> IO ([String], Word64)
+data Options = Options
+  { optMemoryAllowance :: Word64
+  , optPersist :: Bool
+  }
+
+-- | Yields the memory allowance
+parseArgs :: [String] -> IO Options
 parseArgs args =
     if elem "--help" args then do
       hPutStrLn stderr helpMessage
       exitSuccess
     else case parseMemoryAllowance args of
-      Right r -> return r
+      Right (rest, memoryAllowance) -> return Options
+        { optMemoryAllowance = memoryAllowance
+        , optPersist = elem persistentWorkerLabel rest
+        }
       Left e -> do
         hPutStrLn stderr $ "error: " ++ e
         hPutStrLn stderr helpMessage
         exitWith (ExitFailure 1)
+
+persistentWorkerLabel :: String
+persistentWorkerLabel = "--persistent_worker"
 
 memoryAllowanceLabel :: String
 memoryAllowanceLabel = "--memory-allowance"
@@ -28,13 +37,17 @@ memoryAllowanceDefault = 2000
 helpMessage :: String
 helpMessage = unlines
     [ ""
-    , "haskell_module_worker [" ++ memoryAllowanceLabel ++ "] <ghc_opts>"
+    , "haskell_module_worker [" ++ memoryAllowanceLabel
+      ++ "] [" ++ persistentWorkerLabel ++ "]"
     , ""
     ] ++
     unlines (map ("    " ++)
       [ memoryAllowanceLabel
       , "    How much memory the worker is allowed to use in MB. Default: "
         ++ show memoryAllowanceDefault
+      , ""
+      , persistentWorkerLabel
+      , "    Whether to persist and serve multiple request or just one."
       ]
     )
 
