ff4e14143190a580776df6b194432017a5b8046d
Facundo Domínguez <facundo.dominguez@tweag.io>
2022-03-30 19:18:52 -0300

    Read requests with the ProtoClient

--- tools/haskell_module_worker/src/Main.hs
+++ tools/haskell_module_worker/src/Main.hs
@@ -1,21 +1,40 @@
+{-# LANGUAGE LambdaCase #-}
 module Main where
 
-import Compile (runSession, compile)
-import Control.Monad (unless, when)
+import Compile (Status(..), runSession, compile)
+import Control.Exception (try)
+import Control.Monad (forever, unless, when)
 import Control.Monad.IO.Class (liftIO)
+import Data.List (intersperse)
 import Data.Word (Word64)
 import GHC.Stats (getRTSStats, getRTSStatsEnabled, max_live_bytes)
-import Options (parseArgs)
+import Options (Options(..), parseArgs)
+import ProtoClient (WorkRequest(..), createProtoClient, readWorkRequest, writeWorkResponse)
 import System.Environment (getArgs)
+import System.IO (Handle, hSetBinaryMode, stdin, stderr, stdout)
+import System.IO.Error (alreadyInUseErrorType, ioeGetErrorType)
 
 main :: IO ()
 main = do
-    (args, memoryAllowance) <- getArgs >>= parseArgs
-    st <- runSession $ do
-      st <- compile args 0
-      liftIO $ terminateIfUsingTooMuchMemory memoryAllowance
-      return st
-    print st
+    opts <- getArgs >>= parseArgs
+    hSetBinaryMode stdin True
+    hSetBinaryMode stdout True
+    pc <- createProtoClient stdin stdout
+    runSession $ (if optPersist opts then forever else id) $ do
+      wr <- liftIO $ readWorkRequest pc
+      st <- compile (wrArgs wr) (wrVerbosity wr)
+      liftIO $ writeWorkResponse pc (statusExitCode st) (statusOutput st)
+      liftIO $ when (optPersist opts) $
+        terminateIfUsingTooMuchMemory (optMemoryAllowance opts)
+  where
+    statusExitCode = \case { Succeeded{} -> 0; _ -> 1 }
+    statusOutput = \case
+      Succeeded logs -> unlines $ intersperse "" logs
+      CompileErrors logs errs -> unlines $ intersperse "" $ logs ++ errs
+      NonHaskellInputs files ->
+        unlines $ "haskell_module_worker error: non-haskell inputs:" : files
+      NonOneShotCompilation ->
+        "haskell_module_worker error: only on-shot compilation (-c) is supported"
 
 -- | Terminates the worker if it exceeds the memory allowance
 terminateIfUsingTooMuchMemory :: Word64 -> IO ()
