91207f493ecdae34bd71afe6cc881591bb253095
Guillaume Genestier <guillaume.genestier@tweag.io>
2022-08-05 17:29:39 +0200

    Add test on recompilation

---
+++ tests/recompilation/src/B.hs
@@ -0,0 +1,8 @@
+module B where
+import qualified A (f, g, T)
+
+f :: A.T -> A.T
+f x = A.f (A.g x) x
+
+g :: a -> a -> a -> a
+g x y = A.f (A.f x) (A.f x)
---
+++ tests/recompilation/src/C.hs
@@ -0,0 +1,7 @@
+module C where
+import qualified B
+
+data N = Z | S N
+
+f :: a -> a -> a
+f x = B.g x x
