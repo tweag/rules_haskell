3d2228cf0a001be51870d032a29bc903c59d9435
Claudio Bley <claudio.bley@tweag.io>
2023-12-22 09:48:24 +0100

    Use bazelisk from npm for netlify

--- .netlify/build.sh
+++ .netlify/build.sh
@@ -2,24 +2,7 @@
 
 set -eux
 
-export PATH="$HOME/bin:$PATH"
-
-# XXX We don't want to be using the Nixpkgs CC toolchain, because
-# Nixpkgs is not available. But currently we can only override the
-# autoconfigured CC toolchain, not have several (which we would then
-# select via --extra_toolchains). So here's a gross hack that simply
-# patches out the nixpkgs_cc_configure() line.
-#
-# See https://github.com/bazelbuild/bazel/issues/6696.
-awk '
-  BEGIN {del=0}
-  /^nixpkgs_cc_configure\(/ {del=1}
-  del==0 {print}
-  /\)/ {del=0}' WORKSPACE > WORKSPACE.tmp
-  # Note: awk -i inplace not available
-mv WORKSPACE.tmp WORKSPACE
-
-bazel build //docs:api_html
+npx @bazel/bazelisk build //docs:api_html
 mkdir -p public
 unzip -d public bazel-bin/docs/api_html-stardoc.zip
 cp start public
