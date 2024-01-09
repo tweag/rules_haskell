e0ba8bc950f201b05afa6a53d9865318bf8ab1bc
Chuck Grindel <chuck.grindel@gmail.com>
2024-01-09 10:36:43 -0700

    Update the commit for nixpkgs.

--- nixpkgs/default.nix
+++ nixpkgs/default.nix
@@ -1,8 +1,8 @@
 { ... }@args:
 let
-  # 2023-11-30
-  sha256 = "sha256:06m9r85brk6pcghm63ksl2zsx9j1kbagic3fx2p656z20v1bd48z";
-  rev = "781e2a9797ecf0f146e81425c822dca69fe4a348";
+  # 2024-01-09
+  sha256 = "sha256:62eb8e0ff586e410bc873a8e7997ec802709e9a77b03ff2cba644d7019f9599a";
+  rev = "a6671578ffa6336bfd1238f1b2a00662b5ab1b34";
 in
 import (fetchTarball {
   inherit sha256;
