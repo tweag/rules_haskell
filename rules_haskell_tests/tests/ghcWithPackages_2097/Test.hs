2b7eb6341660260cf5551d1a0224c76350c7d7f2
Claudio Bley <claudio.bley@tweag.io>
2024-07-03 10:12:23 +0200

    debug

--- rules_haskell_tests/tests/ghcWithPackages_2097/Test.hs
+++ rules_haskell_tests/tests/ghcWithPackages_2097/Test.hs
@@ -7,5 +7,5 @@ main :: IO ()
 main =  hspec $ do
   it "bazel run ghcWithPackages" $ do
     bazel <- setupTestBazel
-    assertSuccess (bazel ["run", "//:add-one"])
+    assertSuccess (bazel ["run", "-s", "--verbose_failures", "--noenable_bzlmod", "//:add-one"])
     -- assertSuccess (bazel ["cquery", "--output", "build", "@rules_haskell_ghc_nixpkgs_haskell_toolchain//:rts"])
