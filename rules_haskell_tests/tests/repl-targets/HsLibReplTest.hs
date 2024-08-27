3cce8d80c59026b3cecab1a1b88bafd0efb63b0d
Malte Poll <1780588+malt3@users.noreply.github.com>
2024-08-27 11:10:30 +0200

    wip

--- rules_haskell_tests/tests/repl-targets/HsLibReplTest.hs
+++ rules_haskell_tests/tests/repl-targets/HsLibReplTest.hs
@@ -9,4 +9,4 @@ main =  hspec $ do
     bazel <- setupTestBazel
     let p (stdout, _stderr) = lines stdout == ["\"16barbazgen\""]
      in
-       outputSatisfy p (bazel ["run", "//:hs-lib@repl", "--", "-ignore-dot-ghci", "-e", "show (foo 10) ++ bar ++ baz ++ gen"])
+       outputSatisfy p (bazel ["run", "//:hs-lib@repl", "-s", "--", "-ignore-dot-ghci", "-e", "show (foo 10) ++ bar ++ baz ++ gen"])
