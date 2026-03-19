d14b5625537505b3d21e08ca428a2c96db4d1cb4
Kyle Butt <kyle@iteratee.net>
2026-03-19 16:42:55 -0600

    Pass the repo_name when generating paths module
    The previous code used the dirname of the cabal file, which really
    wasn't the correct thing to use. What the runfiles library is really
    looking for is a repository name. Just pass in the repository from the
    label of the cabal library/executable currently being built.

--- haskell/private/generate_cabal_paths_module.py
+++ haskell/private/generate_cabal_paths_module.py
@@ -42,7 +42,7 @@ def normalise_arch(arch):
     return arch
  
 def generate_cabal_paths_module(component_name, ghc_version, is_windows, cabal_basename, cabal_dirname,
-                                ghc, libdir, dynlibdir, bindir, datadir, pkgroot, workspace):
+                                repo_name, ghc, libdir, dynlibdir, bindir, datadir, pkgroot, workspace):
 
     # cabal calls ghc --info to recover the target arch and os, and uses these in path names.
     # https://github.com/haskell/cabal/blob/496d6fcc26779e754523a6cc7576aea49ef8056e/Cabal/src/Distribution/Simple/GHC/Internal.hs#L87
@@ -125,6 +125,9 @@ getSysconfDir = catchIO (getEnv $ packageName++"_sysconfdir") (\_ -> getPrefixDi
 
 """
 
+    if repo_name == "_main":
+        repo_name = workspace + path_separator + cabal_dirname
+
     rebindable_syntax_pragma = ("{-# LANGUAGE NoRebindableSyntax #-}"
                                 if supports_rebindable_syntax else "")
     if supports_cpp:
@@ -177,7 +180,7 @@ version :: Version
 {version_definition}
 
 s = [pathSeparator]
-dataDirWorkspacePath = "{workspace}"++s++"{cabal_dirname}"++s++"_install"++s++"{datadir}"
+dataDirWorkspacePath = "{repo_name}"++s++"_install"++s++"{datadir}"
 packageName = "{component_name}"
 
 libdir = "{libdir}"
@@ -257,6 +260,7 @@ isPathSeparator :: Char -> Bool
         bindir = bindir,
         datadir = datadir,
         cabal_dirname = cabal_dirname,
+        repo_name = repo_name,
         workspace = workspace,
         other_functions = other_functions,
     )
