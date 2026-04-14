2f091723162e52f74d878d88def82237eac54452
Claudio Bley <claudio.bley@tweag.io>
2026-04-14 11:18:12 +0200

    DEBUG netlify 8.x

--- .netlify/build.sh
+++ .netlify/build.sh
@@ -4,7 +4,7 @@ set -eux
 
 rm -rf public
 mkdir public
-export USE_BAZEL_VERSION=7.x
+export USE_BAZEL_VERSION=8.x
 npx @bazel/bazelisk build //docs:api_html
 unzip -d public bazel-bin/docs/api_html-stardoc.zip
 cp start public
