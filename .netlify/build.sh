809842ffd22dc25ce123af0654603f71bb5066db
Claudio Bley <claudio.bley@tweag.io>
2026-04-14 11:15:25 +0200

    DEBUG netlify

--- .netlify/build.sh
+++ .netlify/build.sh
@@ -4,6 +4,7 @@ set -eux
 
 rm -rf public
 mkdir public
-npx @bazel/bazelisk build --spawn_strategy=local //docs:api_html
+export USE_BAZEL_VERSION=7.x
+npx @bazel/bazelisk build //docs:api_html
 unzip -d public bazel-bin/docs/api_html-stardoc.zip
 cp start public
